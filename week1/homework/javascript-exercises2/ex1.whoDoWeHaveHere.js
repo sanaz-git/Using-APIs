d21a5d9e22a90a8e25c8ab6bd79c99c502b78f82
sanaz <szeraati69@gmail.com>
2022-01-01 18:58:38 +0330

    complete ex1.whoDoWeHaveHere

--- week1/homework/javascript-exercises2/ex1.whoDoWeHaveHere.js
+++ week1/homework/javascript-exercises2/ex1.whoDoWeHaveHere.js
@@ -5,17 +5,44 @@
 // Log the received data to the console
 // Incorporate error handling: log to the console the error message
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET','https://www.randomuser.me/api'.true);
-xhr.onreadystatechange = function(){
-    if(xhr.readyState ===4 && xhr.status==200){
-        let response =JSON.parse(xhr.response)
-        console.log(response);
+//XMLHttpRequest
+const url = "https://www.randomuser.me/api";
+function requestWithXHR(url) {
+
+  const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", url, true);
+
+  xhr.send();
+
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      let response = JSON.parse(xhr.response);
+      console.log(`server response :${response.results[0].gender}, ${response.results[0].name.title}
+       ${response.results[0].name.first} ${response.results[0].name.last}, from ${response.results[0].location.country}.` );
+    }else{
+        console.log(`Error ${xhr.status}: ${xhr.statusText}`);
     }
-}
-xhr.onerror = function(){
-    if(xhr.status != 200){
-        console.log(`Error ${xhr.status}: ${xhr.statusText}`)
+  };
+
+  xhr.onerror = function () {
+    if (xhr.status != 200) {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
     }
+  };
 }
-xhr.send();
+
+//axios
+function requestWithAxios(url) {
+  const axios = require("axios");
+  axios
+    .get(url)
+    .then((response) => console.log(`server response :${response.results[0].gender}, ${response.results[0].name.title}
+    ${response.results[0].name.first} ${response.results[0].name.last}, from ${response.results[0].location.country}.`))
+    .catch((error) => console.lpg(error));
+}
+
+requestWithXHR(url);
+requestWithAxios(url);
+
