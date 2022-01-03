d8f3ecd120c71274a72f152ba1a8eaf081ac56a4
sanaz <szeraati69@gmail.com>
2022-01-03 19:43:34 +0330

    update ex2.programmerHumor

--- week1/homework/javascript-exercises2/programmerHumer/ex2.programmerHumor.js
+++ week1/homework/javascript-exercises2/programmerHumer/ex2.programmerHumor.js
@@ -6,30 +6,49 @@
 // Render the img property into an <img> tag in the DOM
 // Incorporate error handling: log to the console the error message
 
-//XMLHttpRequest
+
 const url = "https://xkcd.now.sh/?comic=latest";
 
+//XMLHttpRequest
 function requestWithXHR(url) {
-  const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", url, true);
+    xhr.open("GET", url, true);
 
-  xhr.send();
+    xhr.send();
 
-  xhr.onload = function () {
-    if (xhr.status != 200) {
-      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-    } else {
-      const response = JSON.parse(xhr.response);
-      console.log(response)
-      const imgEl = document.getElementById("imagexhr");
-      imgEl.src = response.img;
-    }
-    xhr.onerror = function () {
-      if (xhr.status != 200) {
-        console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-      }
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+            console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            const response = JSON.parse(xhr.response);
+            console.log(response)
+            const imgEl1 = document.getElementById("imagexhr");
+            imgEl1.src = response.img;
+        }
+        xhr.onerror = function() {
+            if (xhr.status != 200) {
+                console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+            }
+        };
     };
-  };
 }
+
+
+// axios
+function requestWithAxios(url) {
+
+    axios
+        .get(url)
+        .then(response => {
+            console.log(response);
+            const imgEl2 = document.getElementById("imageaxios");
+            imgEl2.src = response.data.img;
+        })
+
+    .catch((error) => console.log(`error : ${error}`));
+}
+
+
 requestWithXHR(url)
+requestWithAxios(url)
