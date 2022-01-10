3eb35c042d2e98ba00ba47f6b338d4336a83011e
sanaz <szeraati69@gmail.com>
2022-01-10 18:54:08 +0330

    update ex2-programmerFun

--- week2/homework/javascript-exercises/ex1-programmerFun/ex1-programmerFun.js
+++ week2/homework/javascript-exercises/ex1-programmerFun/ex1-programmerFun.js
@@ -10,29 +10,30 @@
 // Test error handling, for instance, by temporarily changing the .sh in the url with .shx. There is no server at the modified url,
 //  therefore this should result in a network (DNS) error.
 
+
 const url = 'https://xkcd.now.sh/?comic=latest';
 
 function requestData(url) {
   fetch(url)
-    .then((response) => {
+    .then(response => {
       return response.json();
     })
-    .then((data) => {
-      console.log("Data", data);
+    .then(data => {
+      console.log('Data', data);
       return data;
     })
-    .catch((error) => {
-      console.log("err", error);
+    .catch(error => {
+      console.log('err', error);
     });
 }
 
-function renderImage() {
-  const img = createElement("img");
+function renderImage(data) {
+  const img = document.createElement("img");
   img.src = data.img;
   document.body.appendChild(img);
 }
 
-function renderError() {
+function renderError(error) {
   const h1 = document.createElement("h1");
   h1.innerHTML =`Oops, something went wrong! "${error.message}"`;
   document.body.appendChild(h1);
