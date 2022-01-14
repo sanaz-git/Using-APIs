71db958b1f400fe2cbe65631baefe34cac5024a0
sanaz <szeraati69@gmail.com>
2022-01-14 19:43:20 +0330

    change folder

---
+++ week1/homework/javascript-exercises/HackYourRepo1/index.js
@@ -0,0 +1,27 @@
+function main(){
+  const placeholderRepos = [
+    {
+      name: 'SampleRepo1',
+      description: 'This repository is meant to be a sample',
+      forks: 5,
+      updated: '2020-05-27 12:00:00',
+    },
+    {
+      name: 'AndAnotherOne',
+      description: 'Another sample repo! Can you believe it?',
+      forks: 9,
+      updated: '2020-05-27 12:00:00',
+    },
+    {
+      name: 'HYF-Is-The-Best',
+      description:
+        "This repository contains all things HackYourFuture. That's because HYF is amazing!!!!",
+      forks: 130,
+      updated: '2020-05-27 12:00:00',
+    },
+  ];
+}
+
+main();
+
+
