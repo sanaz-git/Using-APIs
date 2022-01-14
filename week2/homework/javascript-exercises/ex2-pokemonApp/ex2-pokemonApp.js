3cf949dda451ef6929e95528c58fa650e317ed58
sanaz <szeraati69@gmail.com>
2022-01-14 17:49:32 +0330

    update files

--- week2/homework/javascript-exercises/ex2-pokemonApp/ex2-pokemonApp.js
+++ week2/homework/javascript-exercises/ex2-pokemonApp/ex2-pokemonApp.js
@@ -1,42 +1,92 @@
-const url =" https://pokeapi.co/";
+// Complete the four functions provided in the starter `index.js` file:
 
-function fetchData(){
-    fetch(url)
-    .then(response =>{
-       return response.json()  
-    })
-    .then(data =>{
-        return data
-    })
-    .catch(error =>{
-        console.log("err", error); 
-    })
-}
+// `fetchData`: In the `fetchData` function, make use of `fetch` and its Promise
+//   syntax in order to get the data from the public API. Errors (HTTP or network
+//   errors) should be logged to the console.
 
-addEventListener('click', fetchAndPopulatePokemons)
-function fetchAndPopulatePokemons(){
-    const select = document.createElement('select')
-    document.body.appendChild(select);
-}
+// `fetchAndPopulatePokemons`: Use `fetchData()` to load the pokemon data from the
+//   public API and populate the `<select>` element in the DOM.
 
+// `fetchImage`: Use `fetchData()` to fetch the selected image and update the
+//   `<img>` element in the DOM.
 
-function fetchImage() {
-    const img = createElement("img");
-    img.src = data.img;
-    document.body.appendChild(img);
+// `main`: The `main` function orchestrates the other functions. The `main`
+//   function should be executed when the window has finished loading.
+
+// Use async/await and try/catch to handle promises.
+
+// Try and avoid using global variables. As much as possible, try and use function
+// parameters and return values to pass data back and forth.
+
+window.addEventListener("load", main);
+
+function main() {
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+
+  //h1 element
+  const h1 = document.createElement("h1");
+  document.body.appendChild(h1);
+  h1.textContent = "Pokemon Images";
+
+  //buttons element
+  const btn1 = document.createElement("button");
+  document.body.appendChild(btn1);
+  btn1.textContent = "Get Pokemon";
+
+  //select element
+  const select = document.createElement("select");
+  document.body.appendChild(select);
+  select.textContent = "Choose pokemon";
+
+  //image element
+  const img = document.createElement("img");
+  document.body.appendChild(img);
+
+  //When button is clicked
+  btn1.addEventListener("click", fetchAndPopulatePokemon);
+
+  // Add options to select
+  async function fetchAndPopulatePokemon() {
+    try {
+      const data = await fetchData(url);
+      data.results.forEach((element) => {
+        const option = document.createElement("option");
+        option.value = element.name;
+        option.innerHTML = element.name;
+        select.appendChild(option);
+      });
+      // get img
+      select.addEventListener("input", async (e) => {
+
+        let url2 = "";
+        data.results.forEach((element) => {
+          if (element.name == e.target.value) {
+            url2 = element.url;
+          }
+        });
+        fetchImage(url2);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  async function fetchImage(url) {
+    try {
+      const data = await fetchData(url);
+      img.src = data.sprites.front_default;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-//   async function main(){
-//     try {
-//         const fetchedData = await fetch(url)
-//         const parsedData = await fetchedData.json();
-//         fetchImage(parsedData);
-//     }
-
-// catch(error){
-//     console.log(error)
-// }
-// }
-
-// window.addEventListener('load', main);  
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
