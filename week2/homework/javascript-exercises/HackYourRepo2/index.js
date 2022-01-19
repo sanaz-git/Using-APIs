234af91d0c11ae25c4f4aa2f1dd194922f1c7d87
sanaz <szeraati69@gmail.com>
2022-01-19 20:48:24 +0330

    create elements

--- week2/homework/javascript-exercises/HackYourRepo2/index.js
+++ week2/homework/javascript-exercises/HackYourRepo2/index.js
@@ -1,47 +1,98 @@
+const header = document.createElement('header');
+const img = document.createElement('img');
+document.body.appendChild(header);
+header.appendChild(img);
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+container.classList.add('container');
+document.body.appendChild(container);
+
+const section = document.createElement('section');
+document.body.appendChild(section)
+
+//id????
+hyf.classList.add('hyf');
+
+const h1 = document.createElement('h1')
+// append to section with hyf id?
+
 const select = document.getElementById("select");
-const repository = document.getElementById("repository");
-const description = document.getElementById("Description");
-const forks = document.getElementById("Forks");
-const updated = document.getElementById("Updated");
-const card = document.getElementById("card")
+// append to section with hyf id?
+//id?
+selectId.classList.add('selectId')
 
-function main() {
+const option = document.createElement('option')
+select.appendChild(option)
+  
+//id?
+card.classList.add('card')
+container.appendChild(card)
 
-  const placeholderRepos = [
-    {
-      name: 'SampleRepo1',
-      description: 'This repository is meant to be a sample',
-      forks: 5,
-      updated: '2020-05-27 12:00:00',
-    },
-    {
-      name: 'AndAnotherOne',
-      description: 'Another sample repo! Can you believe it?',
-      forks: 9,
-      updated: '2020-05-27 12:00:00',
-    },
-    {
-      name: 'HYF-Is-The-Best',
-      description:
-        "This repository contains all things HackYourFuture. That's because HYF is amazing!!!!",
-      forks: 130,
-      updated: '2020-05-27 12:00:00',
-    },
-  ];
+// have section with reo id??
+//id?
+repository.classList.add('repository')
 
-  for (let i = 0; i < select.clientHeight; i++) {
-    if (select.value == placeholderRepos[i].name) {
-      card.style.display = 'flex';
-      repository.innerHTML = placeholderRepos[i].name;
-      description.innerHTML = placeholderRepos[i].description;
-      forks.innerHTML = placeholderRepos[i].forks;
-      updated.innerHTML = placeholderRepos[i].updated;
-    }
+const h2 = document.createElement('h2')
+// append to section with repo id?
 
-    select.addEventListener('click', main)
-  }
-}
 
-main();
+//ids?
+repository.classList.add('repository')
+description.classList.add('description')
+forks.classList.add('forks')
+updated.classList.add('updated')
+// append to h2 section with repo id?
+
+
+// have section with contribution id??
+//id?
+contribution.classList.add('contribution')
+
+// const repository = document.getElementById("repository");
+// const description = document.getElementById("Description");
+// const forks = document.getElementById("Forks");
+// const updated = document.getElementById("Updated");
+// const card = document.getElementById("card")
+
+// function main() {
+
+//   const placeholderRepos = [
+//     {
+//       name: 'SampleRepo1',
+//       description: 'This repository is meant to be a sample',
+//       forks: 5,
+//       updated: '2020-05-27 12:00:00',
+//     },
+//     {
+//       name: 'AndAnotherOne',
+//       description: 'Another sample repo! Can you believe it?',
+//       forks: 9,
+//       updated: '2020-05-27 12:00:00',
+//     },
+//     {
+//       name: 'HYF-Is-The-Best',
+//       description:
+//         "This repository contains all things HackYourFuture. That's because HYF is amazing!!!!",
+//       forks: 130,
+//       updated: '2020-05-27 12:00:00',
+//     },
+//   ];
+
+//   for (let i = 0; i < select.clientHeight; i++) {
+//     if (select.value == placeholderRepos[i].name) {
+//       card.style.display = 'flex';
+//       repository.innerHTML = placeholderRepos[i].name;
+//       description.innerHTML = placeholderRepos[i].description;
+//       forks.innerHTML = placeholderRepos[i].forks;
+//       updated.innerHTML = placeholderRepos[i].updated;
+//     }
+
+//     select.addEventListener('click', main)
+//   }
+// }
+
+// main();
 
 
