d961dfab4b44db47c35b47f99e6a92882cc688be
sanaz <szeraati69@gmail.com>
2022-01-20 10:01:47 +0330

    edit elements

--- week2/homework/javascript-exercises/HackYourRepo2/index.js
+++ week2/homework/javascript-exercises/HackYourRepo2/index.js
@@ -1,98 +1,61 @@
+//create header
 const header = document.createElement('header');
 const img = document.createElement('img');
+img.src='hyf.png'
+img.alt='hyf'
 document.body.appendChild(header);
 header.appendChild(img);
 
-const div = document.createElement('div');
-document.body.appendChild(div);
+//create container class
+const div1 = document.createElement('div');
+div1.classList.add('container');
+document.body.appendChild(div1);
 
-container.classList.add('container');
-document.body.appendChild(container);
+//create section1
+const section1 = document.createElement('section');
+section1.classList.add('hyf');
+div1.appendChild(section1);
 
-const section = document.createElement('section');
-document.body.appendChild(section)
+const h1 = document.createElement('h1');
+h1.textContent = 'HYF Repositories'
+section1.appendChild(h1)
 
-//id????
-hyf.classList.add('hyf');
+const selectBox = document.createElement('select');
+selectBox.classList.add('selectClass')
+section1.appendChild(selectBox)
 
-const h1 = document.createElement('h1')
-// append to section with hyf id?
+// I think here I must use fetch
+const options = document.createElement('option')
+// select.appendChild(options)
 
-const select = document.getElementById("select");
-// append to section with hyf id?
-//id?
-selectId.classList.add('selectId')
+//create div2
+// const div2 = document.createElement('div')
+// div2.classList.add('card')
+// div1.appendChild(div2)
 
-const option = document.createElement('option')
-select.appendChild(option)
-  
-//id?
-card.classList.add('card')
-container.appendChild(card)
+//create section2
+const section2 = document.createElement('section');
+section2.classList.add('repo');
+div1.appendChild(section2);
 
-// have section with reo id??
-//id?
-repository.classList.add('repository')
+const topic1 = document.createElement('h2');
+topic1.textContent = 'repository'
+section2.appendChild(topic1)
 
-const h2 = document.createElement('h2')
-// append to section with repo id?
+// classes
+// repository.classList.add('repository')
+// description.classList.add('description')
+// forks.classList.add('forks')
+// updated.classList.add('updated')
 
+//create section3
+const section3 = document.createElement('section');
+section3.classList.add('contribution');
+div1.appendChild(section3);
 
-//ids?
-repository.classList.add('repository')
-description.classList.add('description')
-forks.classList.add('forks')
-updated.classList.add('updated')
-// append to h2 section with repo id?
+const topic2 = document.createElement('h2');
+topic2.textContent = 'contribution'
+section3.appendChild(topic2)
 
-
-// have section with contribution id??
-//id?
-contribution.classList.add('contribution')
-
-// const repository = document.getElementById("repository");
-// const description = document.getElementById("Description");
-// const forks = document.getElementById("Forks");
-// const updated = document.getElementById("Updated");
-// const card = document.getElementById("card")
-
-// function main() {
-
-//   const placeholderRepos = [
-//     {
-//       name: 'SampleRepo1',
-//       description: 'This repository is meant to be a sample',
-//       forks: 5,
-//       updated: '2020-05-27 12:00:00',
-//     },
-//     {
-//       name: 'AndAnotherOne',
-//       description: 'Another sample repo! Can you believe it?',
-//       forks: 9,
-//       updated: '2020-05-27 12:00:00',
-//     },
-//     {
-//       name: 'HYF-Is-The-Best',
-//       description:
-//         "This repository contains all things HackYourFuture. That's because HYF is amazing!!!!",
-//       forks: 130,
-//       updated: '2020-05-27 12:00:00',
-//     },
-//   ];
-
-//   for (let i = 0; i < select.clientHeight; i++) {
-//     if (select.value == placeholderRepos[i].name) {
-//       card.style.display = 'flex';
-//       repository.innerHTML = placeholderRepos[i].name;
-//       description.innerHTML = placeholderRepos[i].description;
-//       forks.innerHTML = placeholderRepos[i].forks;
-//       updated.innerHTML = placeholderRepos[i].updated;
-//     }
-
-//     select.addEventListener('click', main)
-//   }
-// }
-
-// main();
 
 
