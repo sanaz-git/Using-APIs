6018d3b13acdc27a25c011117172e08283b4c381
sanaz <szeraati69@gmail.com>
2022-01-30 19:27:56 +0330

    add css

--- week3/homework/javascript-exercises/trivia-app/index.js
+++ week3/homework/javascript-exercises/trivia-app/index.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", main);
 
 function main(){
+  const url = "https://opentdb.com/api.php?amount=5";
+
+
     //create container
     const container = document.createElement('div');
     container.classList.add('cont');
@@ -8,7 +11,7 @@ function main(){
 
     //create section1
     const section1 = document.createElement('div');
-    section1.classList.add('header');
+    section1.classList.add('section1');
     container.appendChild(section1);
 
     const topic1 = document.createElement('h1');
@@ -17,25 +20,26 @@ function main(){
     section1.appendChild(topic1)
 
     const topic2 = document.createElement('h2');
-    topic2.classList.add('topic2')
-    topic2.textContent = 'try your best to figure out the answer.If you really have no clue, click on the question to reveal the answer...'
-    section1.appendChild(topic2)
+    topic2.classList.add('topic2');
+    topic2.textContent = 'try your best to figure out the answer.If you really have no clue, click on the question to reveal the answer...';
+    section1.appendChild(topic2);
 
     //create section2
     const section2 = document.createElement('div');
-    section2.classList.add('trivia-box');
+    section2.classList.add('section2');
     container.appendChild(section2);
 
+    //create question
     const question = document.createElement('h1');
-    question.classList.add('ques');
-    question.textContent = '????????????'
-    section2.appendChild(question)
+    question.classList.add('question');
+    question.textContent = '????????????';
+    section2.appendChild(question);
 
-
-
-
-
-
+    //create answer 
+    const answer = document.createElement('h1');
+    answer.classList.add('answer');
+    answer.textContent = '????????????';
+    section2.appendChild(answer);
 
 
 
@@ -45,3 +49,22 @@ function main(){
 
 }
 
+
+// function fetchData(url){
+//   fetch(url)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error=> console.log(error);
+// }
+
+
+async function fetchData(url){
+  try{
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data)
+  }
+  catch(error){
+     console.log(error);
+  }
+}
