acc7cda266f8261695c3bc4a379d182938ce5c58
sanaz <szeraati69@gmail.com>
2022-02-02 12:57:10 +0330

    convert question to standard characters

--- week3/homework/javascript-exercises/trivia-app/index.js
+++ week3/homework/javascript-exercises/trivia-app/index.js
@@ -2,82 +2,82 @@
 window.addEventListener("load", main);
 
 async function main() {
-    const url = "https://opentdb.com/api.php?amount=5";
-    const listData = await fetchData(url);
+  const url = "https://opentdb.com/api.php?amount=5";
+  const listData = await fetchData(url);
 
 
-    //create container
-    const container = document.createElement('div');
-    container.classList.add('cont');
-    document.body.appendChild(container);
+  //create container
+  const container = document.createElement('div');
+  container.classList.add('cont');
+  document.body.appendChild(container);
 
-    //create section1
-    const section1 = document.createElement('div');
-    section1.classList.add('section1');
-    container.appendChild(section1);
+  //create section1
+  const section1 = document.createElement('div');
+  section1.classList.add('section1');
+  container.appendChild(section1);
 
-    const topic1 = document.createElement('h1');
-    topic1.classList.add('topic1');
-    topic1.textContent = 'Lets play some trivia'
-    section1.appendChild(topic1)
+  const topic1 = document.createElement('h1');
+  topic1.classList.add('topic1');
+  topic1.textContent = 'Lets play some trivia'
+  section1.appendChild(topic1)
 
-    const topic2 = document.createElement('h2');
-    topic2.classList.add('topic2');
-    topic2.textContent = 'try your best to figure out the answer.If you really have no clue, click on the question to reveal the answer...';
-    section1.appendChild(topic2);
+  const topic2 = document.createElement('h2');
+  topic2.classList.add('topic2');
+  topic2.textContent = 'try your best to figure out the answer.If you really have no clue, click on the question to reveal the answer...';
+  section1.appendChild(topic2);
 
-    //create section2
-    const section2 = document.createElement('div');
-    section2.classList.add('section2');
-    container.appendChild(section2);
+  //create section2
+  const section2 = document.createElement('div');
+  section2.classList.add('section2');
+  container.appendChild(section2);
 
-    questionAnswer(listData)
+  questionAnswer(listData)
 
-    function questionAnswer(listData) {
-        listData.results.forEach(element => {
-            //create question
-            const questions = document.createElement('h1');
-            questions.classList.add('questions');
-            questions.textContent = element.question;
-            section2.appendChild(questions);
+  function questionAnswer(listData) {
+    listData.results.forEach(element => {
 
-            //create answer 
-            const answer = document.createElement('h1');
-            answer.classList.add('answer');
-            answer.textContent = element.correct_answer;
-            section2.appendChild(answer);
-            answer.setAttribute('style', 'display:none');
-            
-            //show and hide answer
-            questions.addEventListener('click', () => {
-                    if (answer.style.display == 'block') {
-                        answer.style.display = 'none'
-                    }else{
-                      answer.style.display= 'block'
-                    }
-                })
-          
-            
-        });
+      //convert question to standard characters
+      const convertQuestion = element.question;
 
+      function convert(convertQuestion) {
+        convertQuestion = convertQuestion.replace(/&#039;/g, "'");
+        convertQuestion = convertQuestion.replace(/&quot;/g, '"');
+        return convertQuestion;
+      }
 
+      //create question
+      const questions = document.createElement('h1');
+      questions.classList.add('questions');
+      questions.textContent = convert(convertQuestion) //call convert function here
+      section2.appendChild(questions);
 
-    }
+      //create answer 
+      const answer = document.createElement('h1');
+      answer.classList.add('answer');
+      answer.textContent = element.correct_answer;
+      section2.appendChild(answer);
+      answer.setAttribute('style', 'display:none');
 
+      //show and hide answer
+      questions.addEventListener('click', () => {
+        if (answer.style.display == 'block') {
+          answer.style.display = 'none'
+        } else {
+          answer.style.display = 'block'
+        }
+      })
+    });
+  }
 
-
-
-    //main
+  //main
 }
 
-
-
 async function fetchData(url) {
-    try {
-        let response = await fetch(url);
-        let data = await response.json();
-        return data
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data
+  } catch (error) {
+    console.log(error);
+  }
 }
