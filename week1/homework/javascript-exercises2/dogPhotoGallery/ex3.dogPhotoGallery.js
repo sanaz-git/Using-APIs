57fce753abe245ded19fc0ff0915ce95aad3f38f
Sanaz <szeraati69@gmail.com>
2022-05-18 20:02:30 +0430

    edit css

--- week1/homework/javascript-exercises2/dogPhotoGallery/ex3.dogPhotoGallery.js
+++ week1/homework/javascript-exercises2/dogPhotoGallery/ex3.dogPhotoGallery.js
@@ -11,72 +11,72 @@
 // After receiving the data, append to the <ul> a <li> that contains an <img> element with the dog image
 // Incorporate error handling: log to the console the error message
 
-document.body.setAttribute('style', 'background:#5A69DA; text-align : center;margin-top: 5%; font-family: Arial, Helvetica, sans-serif ')
+document.body.setAttribute(
+  'style',
+  'background:#5A69DA; width: 90%; text-align : center; margin: auto; font-family: zen loop, cursive; justify-content: center; ',
+);
 
-const url = "https://dog.ceo/api/breeds/image/random";
-const select1 = document.getElementById("btn1");
-const select2 = document.getElementById("btn2");
-const ulEl = document.createElement("ulEl");
-const imgSize = '335px';
+const url = 'https://dog.ceo/api/breeds/image/random';
+const select1 = document.getElementById('btn1');
+const select2 = document.getElementById('btn2');
+const ulEl = document.createElement('ulEl');
+const imgSize = '220px';
 ulEl.style.display = 'flex';
 ulEl.style.flexWrap = 'wrap';
+ulEl.style.justifyContent = 'center';
+ulEl.style.margin = 'auto';
 
-
-btn1.setAttribute('style', 'background-color: white; font-family: Arial, Helvetica, sans-serif ; font-size: 15px; width: 160px; height: 40px; padding: 5px; margin: 15px; border-radius: 10px; border: none; display: inline-block ');
-btn2.setAttribute('style', 'background-color: white; font-family: Arial, Helvetica, sans-serif ; font-size: 15px; width: 160px; height: 40px; padding: 5px; margin: 15px; border-radius: 10px; border: none;  ');
-
- 
 //XMlHttpRequest
 
-select1.addEventListener("click", () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true)
-    xhr.send();
+select1.addEventListener('click', () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.send();
 
-    xhr.onload = function () {
-        if (xhr.status != 200) {
-            console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-        } else {
-            const response = JSON.parse(xhr.response);
-            console.log(response) 
-            const liEl = document.createElement('li');
-            const imgEl = document.createElement('img');
-            document.body.appendChild(ulEl);
-            imgEl.src = `${response.message}`;
-            imgEl.style.height = imgSize;
-            imgEl.style.width = imgSize;
-            liEl.style.listStyle = 'none';
-            liEl.appendChild(imgEl);
-            ulEl.appendChild(liEl);
-        }
-        xhr.onerror = function () {
-            if (xhr.status != 200) {
-            console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-            }
-        };
-        };
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      const response = JSON.parse(xhr.response);
+      console.log(response);
+      const liEl = document.createElement('li');
+      const imgEl = document.createElement('img');
+      document.body.appendChild(ulEl);
+      imgEl.src = `${response.message}`;
+      imgEl.style.height = imgSize;
+      imgEl.style.width = imgSize;
+      imgEl.style.margin = '10px 5px 5px 10px';
+      imgEl.style.borderRadius = '20px';
+      liEl.style.listStyle = 'none';
+      liEl.appendChild(imgEl);
+      ulEl.appendChild(liEl);
+    }
+    xhr.onerror = function () {
+      if (xhr.status != 200) {
+        console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+      }
+    };
+  };
 });
 
 // Axios
-select2.addEventListener("click", () => {
+select2.addEventListener('click', () => {
   axios
-      .get(url)
-      .then(response => {
-          console.log(response) 
-          document.body.appendChild(ulEl);
-          const liEl = document.createElement('li');
-          const imgEl = document.createElement('img');
-          imgEl.src = response.data.message;
-             imgEl.style.height = imgSize;
-             imgEl.style.width = imgSize;
-             liEl.style.listStyle = 'none';
-          liEl.appendChild(imgEl);
-          ulEl.appendChild(liEl);
-      })
+    .get(url)
+    .then((response) => {
+      console.log(response);
+      document.body.appendChild(ulEl);
+      const liEl = document.createElement('li');
+      const imgEl = document.createElement('img');
+      imgEl.src = response.data.message;
+      imgEl.style.height = imgSize;
+      imgEl.style.width = imgSize;
+      imgEl.style.margin = '10px 5px 5px 10px';
+      imgEl.style.borderRadius = '20px';
+      liEl.style.listStyle = 'none';
+      liEl.appendChild(imgEl);
+      ulEl.appendChild(liEl);
+    })
 
-  .catch((error) => console.log(`error : ${error}`));
-})
-
-
-
-
+    .catch((error) => console.log(`error : ${error}`));
+});
