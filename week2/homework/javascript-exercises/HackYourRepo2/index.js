74ef9165904b157bb9c43de14552de45e41d6781
Sanaz <szeraati69@gmail.com>
2022-05-18 21:31:14 +0430

    fix project

--- week2/homework/javascript-exercises/HackYourRepo2/index.js
+++ week2/homework/javascript-exercises/HackYourRepo2/index.js
@@ -1,11 +1,11 @@
-window.addEventListener("load", main);
+window.addEventListener('load', main);
 
 async function main() {
   //create header
   const header = document.createElement('header');
   const img = document.createElement('img');
-  img.src = 'hyf.png'
-  img.alt = 'hyf'
+  img.src = 'hyf.png';
+  img.alt = 'hyf';
   document.body.appendChild(header);
   header.appendChild(img);
 
@@ -20,23 +20,22 @@ async function main() {
   div1.appendChild(section1);
 
   const h1 = document.createElement('h1');
-  h1.textContent = 'HYF Repositories'
-  section1.appendChild(h1)
+  h1.textContent = 'HYF Repositories';
+  section1.appendChild(h1);
 
   const selectBox = document.createElement('select');
-  selectBox.classList.add('selectClass')
-  section1.appendChild(selectBox)
+  selectBox.classList.add('selectClass');
+  section1.appendChild(selectBox);
 
   const option = document.createElement('option');
   selectBox.appendChild(option);
   option.textContent = '-- Choose an Option --';
 
-
   // create div2
-  const div2 = document.createElement('div')
-  div2.classList.add('card')
-  div2.style.display= 'none'
-  div1.appendChild(div2)
+  const div2 = document.createElement('div');
+  div2.classList.add('card');
+  div2.style.display = 'none';
+  div1.appendChild(div2);
 
   //create section2
   const section2 = document.createElement('section');
@@ -45,27 +44,27 @@ async function main() {
 
   // details of section2
   const topic1 = document.createElement('h2');
-  topic1.textContent = 'repository: '
-  section2.appendChild(topic1);;
-  const repository = document.createElement('span')
+  topic1.textContent = 'repository: ';
+  section2.appendChild(topic1);
+  const repository = document.createElement('span');
   topic1.appendChild(repository);
-  
+
   const topic2 = document.createElement('h2');
-  topic2.textContent = 'description: '
-  section2.appendChild(topic2);;
-  const description = document.createElement('span')
+  topic2.textContent = 'description: ';
+  section2.appendChild(topic2);
+  const description = document.createElement('span');
   topic2.appendChild(description);
-  
+
   const topic3 = document.createElement('h2');
-  topic3.textContent = 'forks: '
-  section2.appendChild(topic3);;
-  const forks = document.createElement('span')
+  topic3.textContent = 'forks: ';
+  section2.appendChild(topic3);
+  const forks = document.createElement('span');
   topic3.appendChild(forks);
 
   const topic4 = document.createElement('h2');
-  topic4.textContent = 'updated: '
-  section2.appendChild(topic4);;
-  const updated = document.createElement('span')
+  topic4.textContent = 'updated: ';
+  section2.appendChild(topic4);
+  const updated = document.createElement('span');
   topic4.appendChild(updated);
 
   //create section3
@@ -76,88 +75,99 @@ async function main() {
   const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
   const listData = await fetchData(url);
-  console.log(listData)
+  console.log(listData);
 
-  populateSelect(listData)
+  populateSelect(listData);
 
-  selectBox.addEventListener('input', (e) =>{
-  section3.innerHTML = '';
-  let url2 ="";
+  selectBox.addEventListener('input', (e) => {
+    section3.innerHTML = '';
+    let url2 = '';
 
-  listData.forEach((element) => {
-    if (element.name == e.target.value) {
+    listData.forEach((element) => {
+      if (element.name == e.target.value) {
+        repository.textContent = element.name;
+        description.textContent = element.description;
+        forks.textContent = element.forks;
+        updated.textContent = element.updated_at;
 
-      repository.textContent = element.name;
-      description.textContent = element.description;
-      forks.textContent = element.forks;
-      updated.textContent = element.updated_at;
+        const topic5 = document.createElement('h2');
+        topic5.textContent = 'contributions';
+        topic5.setAttribute(
+          'style',
+          'padding:10px; border:1px solid lightgray; text-align:center; font-size:20px; color:gray',
+        );
 
-      const topic5 = document.createElement('h2')
-      topic5.textContent = 'contributions'
-      topic5.setAttribute('style' , 'padding:10px; border:1px solid lightgray; text-align:center; font-size:20px; color:gray')
-      
-      section3.appendChild(topic5)
-    
-      url2 = element.contributors_url; 
-      
-    }
+        section3.appendChild(topic5);
+
+        url2 = element.contributors_url;
+      }
+    });
+    fetchContributions(url2);
   });
-  fetchContributions(url2)
-});
 
-async function fetchContributions(url2){
-  try{
-    const data = await fetchData(url2);
-    data.forEach(element => {
+  async function fetchContributions(url2) {
+    try {
+      const data = await fetchData(url2);
+      data.forEach((element) => {
+        div2.style.display = 'block';
 
-      // contDetails.parentNode.removeChild(contDetails)
+        const contDetails = document.createElement('div');
+        contDetails.setAttribute(
+          'style',
+          'display:flex; flex:right; padding: 10px',
+        );
+        section3.appendChild(contDetails);
 
-    div2.style.display= 'block' 
+        const img2 = document.createElement('img');
+        img2.src = element.avatar_url;
+        img2.setAttribute(
+          'style',
+          ' padding-top:5px; width: 15%; height: 15%; border-radius:50%; margin-right:20px; margin-left:30px',
+        );
+        contDetails.appendChild(img2);
 
-    const contDetails = document.createElement('div')
-    contDetails.setAttribute('style', 'display:flex; flex:right')
-    section3.appendChild(contDetails);
+        const contName = document.createElement('h2');
+        contName.textContent = element.login;
+        contName.setAttribute(
+          'style',
+          'min-width:30%; font-size:1vw; text-align:center; height:10%; margin-top:20px; margin-left:10%; border-style: groove; border-radius:10%',
+        );
+        contDetails.appendChild(contName);
 
-    const img2 = document.createElement('img');
-    img2.src = element.avatar_url;
-    img2.setAttribute('style', 'position: relative; padding-top:5px; width: 20%; height: 20%; border-radius:50% ');
-    contDetails.appendChild(img2);
+        const contNum = document.createElement('h2');
+        contNum.textContent = element.contributions;
+        contNum.setAttribute(
+          'style',
+          'min-width:10%; font-size:1vw; text-align:center; height:10%; margin-top:20px; margin-left:10%;  border-style: groove; border-radius:10%',
+        );
+        contDetails.appendChild(contNum);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    const contName = document.createElement('h2');
-    contName.textContent = element.login;
-    contName.setAttribute('style', 'width:150px; text-align:center; height:25px; margin-top:20px; margin-left:30px; border-style: groove; border-radius:10%')
-    contDetails.appendChild(contName);
-
-    const contNum = document.createElement('h2');
-    contNum.textContent = element.contributions
-    contNum.setAttribute('style', 'background: white; color: black; text-align: center; width: 30px; height:25px; margin-top:20px; border-style: groove; border-radius:10% ; margin-bottom: 10px; margin-left: 50px; margin-right:5px')
-    contDetails.appendChild(contNum);
-   
-    })  
-  } catch(error){
-    console.log(error)
+  function populateSelect(listData) {
+    listData.forEach((element) => {
+      const option = document.createElement('option');
+      option.value = element.name;
+      option.innerHTML = element.name;
+      selectBox.appendChild(option);
+    });
   }
 }
 
-  function populateSelect(listData) {    
-    listData.forEach (element => {
-    const option = document.createElement('option');
-    option.value = element.name;
-    option.innerHTML = element.name;
-    selectBox.appendChild(option);  
-  });
-  }
-  }
-
-   
 async function fetchData(url) {
-  try {    
+  try {
     const response = await fetch(url);
     const data = await response.json();
-    return data
+    return data;
   } catch (error) {
     const err = document.createElement('h2');
-    err.setAttribute('style', 'background-color: #4a707a;width: 400px; height: 100px;padding: 15px;position: absolute;top: 50%;left: 50%;margin: -70px 0 0 -200px; color: white; font-family: "Space Mono", monospace; text-align: center ')
+    err.setAttribute(
+      'style',
+      'background-color: #4a707a;width: 400px; height: 100px;padding: 15px;position: absolute;top: 50%;left: 50%;margin: -70px 0 0 -200px; color: white; font-family: "Space Mono", monospace; text-align: center ',
+    );
     err.textContent = `network request failed ! "${error.message}"`;
     document.body.appendChild(err);
   }
