7a5ed25093d67d35e92b40eab307ee5ae91d643e
Sanaz <szeraati69@gmail.com>
2022-05-18 18:48:13 +0430

    update

--- week1/homework/javascript-exercises2/ex1.whoDoWeHaveHere.js
+++ week1/homework/javascript-exercises2/ex1.whoDoWeHaveHere.js
@@ -6,52 +6,51 @@
 // Incorporate error handling: log to the console the error message
 
 //XMLHttpRequest
-const url = "https://www.randomuser.me/api";
+const url = 'https://www.randomuser.me/api';
 
 function requestWithXHR(url) {
+  const xhr = new XMLHttpRequest();
 
-    const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
 
-    xhr.open("GET", url, true);
+  xhr.send();
 
-    xhr.send();
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      let response = JSON.parse(xhr.response);
+      console.log(response.results[0].name.first);
+    } else {
+      console.log(`Error ${xhr.status}: ${xhr.response}`);
+    }
+  };
 
-    xhr.onload = function() {
-        if (xhr.status == 200) {
-            let response = JSON.parse(xhr.response);
-            console.log(response.results[0].name.first);
-        } else {
-            console.log(`Error ${xhr.status}: ${xhr.response}`);
-        }
-    };
-
-    xhr.onerror = function() {
-        if (xhr.status != 200) {
-            console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-        }
-    };
+  xhr.onerror = function () {
+    if (xhr.status != 200) {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    }
+  };
 }
 
 // axios
 function requestWithAxios(url) {
-
-    axios
-        .get(url)
-        .then((response) => console.log(response.data.results[0].name.first))
-        .catch((error) => console.log(`error : ${error}`));
+  axios
+    .get(url)
+    .then((response) => console.log(response.data.results[0].name.first))
+    .catch((error) => console.log(`error : ${error}`));
 }
 
+//fetch
 function fetchRequest(url) {
-    fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 requestWithXHR(url);
