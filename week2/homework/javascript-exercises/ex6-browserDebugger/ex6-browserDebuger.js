85af3fc9ae3e09384db602054d76eae150f8bad6
Sanaz <szeraati69@gmail.com>
2022-05-28 22:37:50 +0430

    add css

--- week2/homework/javascript-exercises/ex6-browserDebugger/ex6-browserDebuger.js
+++ week2/homework/javascript-exercises/ex6-browserDebugger/ex6-browserDebuger.js
@@ -1,51 +1,59 @@
 async function getData(url) {
-    const response = await fetch(url);
-    return response.json();
-  }
-  
-  function createAndAppend(name, parent, options = {}) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    Object.entries(options).forEach(([key, value]) => {
-      if (key === 'text') {
-        elem.textContent = value;
-      } else {
-        elem.setAttribute(key, value);
-      }
-    });
-    return elem;
-  }
-  
-  function addTableRow(table, label, value) {
-    const tr = createAndAppend('tr', table);
-    createAndAppend('th', tr, { text: label });
-    createAndAppend('td', tr, { text: value });
-  }
-  
-  function renderLaureate(ul, { knownName, birth, death }) {
-    const li = createAndAppend('li', ul);
-    const table = createAndAppend('table', li);
-    addTableRow(table, 'Name', knownName.en);
-    addTableRow(table, 'Birth', `${birth.date}, ${birth.place.locationString.en}`);
-    if (death) {
-      addTableRow(table, 'Death', `${death.date}, ${death.place.locationString.en}`);
+  const response = await fetch(url);
+  return response.json();
+}
+
+function createAndAppend(name, parent, options = {}) {
+  const elem = document.createElement(name);
+  parent.appendChild(elem);
+  Object.entries(options).forEach(([key, value]) => {
+    if (key === 'text') {
+      elem.textContent = value;
+    } else {
+      elem.setAttribute(key, value);
     }
+  });
+  return elem;
+}
+
+function addTableRow(table, label, value) {
+  const tr = createAndAppend('tr', table);
+  createAndAppend('th', tr, { text: label });
+  createAndAppend('td', tr, { text: value });
+}
+
+function renderLaureate(ul, { knownName, birth, death }) {
+  const li = createAndAppend('li', ul);
+  const table = createAndAppend('table', li);
+  addTableRow(table, 'Name:', knownName.en);
+  addTableRow(
+    table,
+    'Birth:',
+    `  ${birth.date} ${birth.place.locationString.en}`,
+  );
+  if (death) {
+    addTableRow(
+      table,
+      'Death:',
+      `${death.date}, ${death.place.locationString.en}`,
+    );
   }
-  
-  function renderLaureates(laureates) {
-    const ul = createAndAppend('ul', document.body);
-    laureates.forEach((laureate) => renderLaureate(ul, laureate));
+}
+
+function renderLaureates(laureates) {
+  const ul = createAndAppend('ul', document.body);
+  laureates.forEach((laureate) => renderLaureate(ul, laureate));
+}
+
+async function fetchAndRender() {
+  try {
+    const { laureates } = await getData(
+      'https://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en',
+    );
+    renderLaureates(laureates);
+  } catch (err) {
+    console.error(`Something went wrong: ${err.message}`);
   }
-  
-  async function fetchAndRender() {
-    try {
-      const { laureates } = await getData(
-        'https://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
-      );
-      renderLaureates(laureates);
-    } catch (err) {
-      console.error(`Something went wrong: ${err.message}`);
-    }
-  }
-  
-  window.addEventListener('load', fetchAndRender);
+}
+
+window.addEventListener('load', fetchAndRender);
