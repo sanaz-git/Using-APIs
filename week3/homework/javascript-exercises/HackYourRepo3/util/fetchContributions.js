cceef1c59477194878686a660befd2202cb63c34
Sanaz <szeraati69@gmail.com>
2022-05-28 21:22:06 +0430

    add first page

--- week3/homework/javascript-exercises/HackYourRepo3/util/fetchContributions.js
+++ week3/homework/javascript-exercises/HackYourRepo3/util/fetchContributions.js
@@ -12,6 +12,7 @@ export async function fetchContributions(url2, section3) {
     section3.appendChild(topic5);
 
     const data = await fetchData(url2);
+
     console.log(data);
 
     const arrayNumber = data.length;
@@ -24,6 +25,40 @@ export async function fetchContributions(url2, section3) {
     contDetails.classList.add('contDetails');
     contDetails.setAttribute('style', ' flex-direction:row; flex-wrap: wrap');
     section3.appendChild(contDetails);
+
+    let firstPage = data.slice(0, 5);
+    console.log(firstPage);
+
+    firstPage.forEach((element) => {
+      const containsDiv2 = document.createElement('div');
+      containsDiv2.setAttribute('style', 'display:flex;  padding: 10px ');
+      containsDiv2.style.padding = '10px';
+      contDetails.appendChild(containsDiv2);
+
+      const img3 = document.createElement('img');
+      img3.src = element.avatar_url;
+      img3.setAttribute(
+        'style',
+        ' display:block , padding-top:5px; width: 15%; height: 15%; border-radius:50%; margin-right:20px; margin-left:30px',
+      );
+      containsDiv2.appendChild(img3);
+
+      const contName2 = document.createElement('h2');
+      contName2.textContent = element.login;
+      contName2.setAttribute(
+        'style',
+        'width:140px; font-size: 1.2vw; text-align:center; height:25px; padding-bottom:5px; border-style: groove; border-radius:10%; margin-top:10px; margin-left:10%',
+      );
+      containsDiv2.appendChild(contName2);
+
+      const contNum2 = document.createElement('h2');
+      contNum2.textContent = element.contributions;
+      contNum2.setAttribute(
+        'style',
+        'background: white;font-size: 1.2vw; color: black; text-align: center; width: 30px; height:25px;padding-bottom:5px;border-style: groove; border-radius:10% ; margin-left: 10%; margin-right:10px;margin-top:10px ',
+      );
+      containsDiv2.appendChild(contNum2);
+    });
 
     createButton(pageNumber, section3, contDetails, data);
   } catch (error) {
--- week3/homework/javascript-exercises/HackYourRepo3/util/script.js
+++ week3/homework/javascript-exercises/HackYourRepo3/util/script.js
@@ -3,40 +3,54 @@ import { fetchData } from './fetchData.js';
 import { populateSelect } from './populateSelect.js';
 import { fetchContributions } from './fetchContributions.js';
 
-window.addEventListener("load", main);
+window.addEventListener('load', main);
 
 async function main() {
-  const { selectBox, section3, repository, description, forks, updated, img2, contNum, contName, contDetails, div2} = addElements()
+  const {
+    selectBox,
+    section3,
+    repository,
+    description,
+    forks,
+    updated,
+    img2,
+    contNum,
+    contName,
+    contDetails,
+    div2,
+  } = addElements();
   const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-  console.log(selectBox, section3, repository, description, forks, updated)
+  console.log(selectBox, section3, repository, description, forks, updated);
 
   const listData = await fetchData(url);
-  console.log(listData)
+  console.log(listData);
 
-  populateSelect(listData, selectBox)
- 
+  populateSelect(listData, selectBox);
 
   selectBox.addEventListener('input', (e) => {
     section3.innerHTML = '';
-    let url2 = "";
+    let url2 = '';
 
     listData.forEach((element) => {
       if (element.name == e.target.value) {
-
         repository.textContent = element.name;
         description.textContent = element.description;
         forks.textContent = element.forks;
         updated.textContent = element.updated_at;
 
         url2 = element.contributors_url;
-
       }
     });
-    
-    fetchContributions(url2, section3, img2, contNum, contName, contDetails, div2)
-    div2.style.display = 'block'
 
+    fetchContributions(
+      url2,
+      section3,
+      img2,
+      contNum,
+      contName,
+      contDetails,
+      div2,
+    );
+    div2.style.display = 'block';
   });
-
-  
 }
